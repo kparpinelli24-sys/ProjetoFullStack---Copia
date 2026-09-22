37105df2cd590e57b46fb5befbd20e8ff080215c
kparpinelli24-sys <k.parpinelli.24@gmail.com>
2026-09-22 11:21:54 -0300

    feat: add home component and styles for planet catalog

---
+++ frontend/src/Home/home.jsx
@@ -0,0 +1,24 @@
+import '/home.css'
+
+const catalogo = [
+  {
+    nome: 'Kepler-452b',
+    imagem: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Kepler-452b_artist_concept.jpg?utm_source=pt.wikipedia.org&utm_campaign=index&utm_content=original&#39'
+  },
+  {
+    nome: 'HD 189733b',
+    imagem: 'https://s2-oglobo.glbimg.com/TilppykMTZI_YY06G0yTWmwIDUs=/0x0:1200x900/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2025/k/G/ANvMrnTWCKiKiA1HaBRw/planeta-nasa.webp'
+  },
+  {
+    nome: 'Proxima Centauri b',
+    imagem: 'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?auto=format&fit=crop&w=100&q=80&#39'
+  },
+  {
+    nome: 'TRAPPIST-1e',
+    imagem: 'https://images.unsplash.com/photo-1504333638930-c8787321eee0?auto=format&fit=crop&w=100&q=80&#39'
+  },
+  {
+    nome: 'Nebulosa do Caranguejo',
+    imagem: 'https://images.unsplash.com/photo-1516339901601-2e1b62dc0c45?auto=format&fit=crop&w=100&q=80&#39'
+  }
+]
